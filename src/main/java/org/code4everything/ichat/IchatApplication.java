4bedd99e3340124e9bb73d884e011fe8dcdb8dd9
zhazhapan <tao@zhazhapan.com>
2018-07-25 21:08:39 +0800

    initial commit

---
+++ src/main/java/org/code4everything/ichat/IchatApplication.java
@@ -0,0 +1,16 @@
+package org.code4everything.ichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pantao
+ * @since 2018/7/25
+ */
+@SpringBootApplication
+public class IchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IchatApplication.class, args);
+    }
+}
---
+++ src/test/java/org/code4everything/ichat/IchatApplicationTests.java
@@ -0,0 +1,16 @@
+package org.code4everything.ichat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IchatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
